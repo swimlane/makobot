43d0b17a81d03c90c94a2bdfe7323926e99e9249
Michael Lavers <kolanos@ip-10-0-0-2.ec2.internal>
2016-03-30 15:19:49 -0700

    Plugging env vars

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var builder = require('botbuilder');
 var restify = require('restify');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+var bot = new builder.BotConnectorBot({appId: process.env.APP_ID, appSecret: process.env.APP_SECRET});
 bot.add('/', function (session) {
     session.send('Hello World');
 });
@@ -10,6 +10,6 @@ bot.add('/', function (session) {
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
