15d615dc89cc3bc6d81fc338aa1d83de410cb168
Michael Lavers <kolanos@ip-10-0-0-2.ec2.internal>
2016-03-30 14:50:42 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+bot.add('/', function (session) {
+    session.send('Hello World');
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
