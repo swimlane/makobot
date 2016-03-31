c5638ca98be8694dbb64fa4dfda5976b171e9ca5
Michael Lavers <kolanos@ip-10-0-0-2.ec2.internal>
2016-03-30 17:46:00 -0700

    Minor changes

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var bot = new builder.BotConnectorBot({
     appId: process.env.APP_ID,
     appSecret: process.env.APP_SECRET
 });
+
 bot.add('/', function (session) {
     session.send('Hello World');
 });
 
 // Setup Restify Server
 var server = restify.createServer();
+
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
