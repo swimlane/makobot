74548c6db4be5c2a7ab9319bc36e69644dcac088
Michael Lavers <kolanos@ip-10-0-0-2.ec2.internal>
2016-04-05 12:40:15 -0700

    Adding X-Force IP reputation reports

--- makobot/plugins/ip.py
+++ makobot/plugins/ip.py
@@ -2,15 +2,64 @@ import re
 
 from slackbot.bot import listen_to, respond_to
 
+
+from makobot import local_settings as settings
+from makobot.libs.xforce import XForce
+
 IP_REGEX = re.compile(
     r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})')
 
 
+def extract_ips(message):
+    """REturns the IPs contained within the message text"""
+    return IP_REGEX.findall(message.body.get('text', ''))
+
+
+def xforce_ipr(*ips):
+    """Returns the IP reputations for the provided IPs from IBM x-Force"""
+    iprs = []
+    if not settings.XFORCE_API_KEY or not settings.XFORCE_PASSWORD:
+        return iprs
+    xforce = XForce(settings.XFORCE_API_KEY, settings.XFORCE_PASSWORD)
+    for ip in ips:
+        iprs.append(xforce.ipr(ip))
+    return iprs
+
+
+def ipr_report(ipr, inline=False):
+    """REturns an IP reputation report"""
+    report = []
+    if 'ip' in report:
+        report.append('X-Force IP Reputation for *%s*' % ipr['ip'])
+    if 'score' in ipr:
+        report.append('*Score:* %s' % ipr['score'])
+        report.append('*Risk Level:* %s' % XForce.risk_level(ipr['score']))
+    if 'reason' in ipr:
+        report.append('*Reason:* %s' % ipr['reason'])
+    if 'cats' in ipr and ipr['cats']:
+        report.append('*Categories:* %s' % ', '.join([
+            '%s (%s)' % (k, v) for k, v in ipr['cats'].items()]))
+    if inline:
+        return ' '.j(report)
+    return '\n'.join(report)
+
+
 @respond_to(IP_REGEX)
 def ip_active(message):
-    pass
+    ips = extract_ips(message)
+    iprs = xforce_ipr(ips)
+    for ipr in iprs:
+        message.reply(ipr_report(ipr))
 
 
 @listen_to(IP_REGEX)
 def ip_passive(message):
-    pass
+    ips = extract_ips(message)
+    iprs = xforce_ipr(ips)
+    react = False
+    for ipr in iprs:
+        if 'score' in ipr and ipr['score'] >= 3:
+            message.send(ipr_report(ipr, inline=True))
+            react = True
+    if react:
+        message.react('warning')
