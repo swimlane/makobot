1905535c2422b3c1a56c3bc4e41b9bcf173ebca5
Michael Lavers <kolanos@ip-10-0-0-2.ec2.internal>
2016-04-07 10:53:03 -0700

    Disable md5 listeners for now

--- makobot/plugins/md5.py
+++ makobot/plugins/md5.py
@@ -11,11 +11,9 @@ def extract_md5s(message):
     return set(MD5_REGEX.findall(message.body.get('text', '')))
 
 
-@respond_to(MD5_REGEX)
 def md5_active(message):
     message.reply('I will provide info about this md5')
 
 
-@listen_to(MD5_REGEX)
 def md5_passive(message):
     message.send('I will provide info about this md5')
