ea3bbb3ac6bb3b712d2534fc7bc85e8ceba8ef52
Michael Lavers <kolanos@Michaels-MacBook-Pro.local>
2016-04-11 10:52:22 -0700

    Adding a setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='makobot',
+    description='A Slack bot to monitor for potential security rissks.',
+    author='Swimlane LLC',
+    author_email='info@swimlane.com',
+    url='https://github.com/swimlane/makobot',
+    version='0.1.0',
+    license='MIT',
+    install_requires=['requests', 'slackbot'],
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    packages=find_packages(),
+    keywords='slack security bot malware ip url reputation',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+    ]
+)
