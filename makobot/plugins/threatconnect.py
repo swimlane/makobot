4ebd41ae8a4da349b10d0242b47bebb446afe839
Michael Lavers <kolanos@gmail.com>
2016-05-20 12:34:31 -0700

    Better check for rating attribute

--- makobot/plugins/threatconnect.py
+++ makobot/plugins/threatconnect.py
@@ -49,7 +49,8 @@ class ThreatConnectPlugin(Plugin):
     def react(self):
         if not any(self.reports.values()):
             return
-        rating = max([r.rating for r in self.reports.values() if r.rating])
+        rating = max([r.rating for r in self.reports.values()
+                      if r and hasattr(r, 'rating') and r.rating])
         return self.reaction(rating)
 
     def reaction(self, score):
