763b1ef0e22f36ce37f37dd7356609ee2d7b4964
Michael Lavers <kolanos@gmail.com>
2016-05-20 15:19:12 -0700

    Upping version v0.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Swimlane LLC',
     author_email='info@swimlane.com',
     url='https://github.com/swimlane/makobot',
-    version='0.3.0',
+    version='0.3.1',
     license='MIT',
     install_requires=['requests', 'slackbot', 'threatconnect'],
     tests_require=['mock', 'nose'],
